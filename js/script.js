f5e6b65d0258e5048370e5b086bd2f9804954a40
JaePewu <one.sweet.dreams@gmail.com>
2026-09-04 17:21:29 -0400

    Se agregan  validaciones al formulario de registro (contraseña)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-//Capturar elemtos de formulari
+//Elemtos de formulario
 const formularioRegistro = document.getElementById("formularioRegistro");
 const nombre = document.getElementById("nombre");
 const errorNombre = document.getElementById("errorNombre");
 
 const correo = document.getElementById("correo");
 const errorCorreo = document.getElementById("errorCorreo");
+
+const contrasena = document.getElementById("contrasena");
+const errorContrasena = document.getElementById("errorContrasena");
 
 //Validación
 formularioRegistro.addEventListener("submit", function (event) {
@@ -13,9 +16,14 @@ formularioRegistro.addEventListener("submit", function (event) {
   //Valores ingresados
   const valorNombre = nombre.value.trim();
   const valorCorreo = correo.value.trim();
+  const valorContrasena = contrasena.value;
 
   const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s]+$/;
   const formatoCorreo = /^[A-Za-z0-9._%+-]+@duoc\.cl$/i;
+  const tieneMayuscula = /[A-Z]/;
+  const tieneMinuscula = /[a-z]/;
+  const tieneNumero = /[0-9]/;
+  const tieneEspecial = /[@#$%!]/;
 
   // Validación nombre completo
   if (valorNombre === "") {
@@ -29,7 +37,7 @@ formularioRegistro.addEventListener("submit", function (event) {
     errorNombre.textContent = "";
   }
 
-  // Validación correo electrónico
+  //Validación correo electrónico
   if (valorCorreo === "") {
     errorCorreo.textContent = "El correo electrónico es obligatorio.";
   } else if (valorCorreo.length > 60) {
@@ -38,5 +46,27 @@ formularioRegistro.addEventListener("submit", function (event) {
     errorCorreo.textContent = "Ingresa un correo válido.";
   } else {
     errorCorreo.textContent = "";
+  }
+
+  //Validación de contraseña
+  if (valorContrasena === "") {
+    errorContrasena.textContent = "La contraseña es obligatoria.";
+  } else if (valorContrasena.length < 10) {
+    errorContrasena.textContent =
+      "La contraseña debe tener al menos 10 caracteres.";
+  } else if (!tieneMayuscula.test(valorContrasena)) {
+    errorContrasena.textContent =
+      "La contraseña debe incluir al menos una letra mayúscula.";
+  } else if (!tieneMinuscula.test(valorContrasena)) {
+    errorContrasena.textContent =
+      "La contraseña debe incluir al menos una letra minúscula.";
+  } else if (!tieneNumero.test(valorContrasena)) {
+    errorContrasena.textContent =
+      "La contraseña debe incluir al menos un número.";
+  } else if (!tieneEspecial.test(valorContrasena)) {
+    errorContrasena.textContent =
+      "La contraseña debe incluir un carácter especial (@, #, $, % o !).";
+  } else {
+    errorContrasena.textContent = "";
   }
 });
